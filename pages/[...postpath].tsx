68d8d3ab811b16d6fea313ae9dd5bfbf4657792b
bilal3569 <157413693+bilal3569@users.noreply.github.com>
2024-01-23 00:39:35 +0500

    Update [...postpath].tsx
    graphql from redirection url

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       redirect: {
         permanent: false,
         destination: `${
-          endpoint.replace(/(\/graphql\/)/, "/") + encodeURI(path as string)
+          endpoint.replace(/(\/\/)/, "/") + encodeURI(path as string)
         }`,
       },
     };
